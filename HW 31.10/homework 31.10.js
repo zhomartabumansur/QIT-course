f2162bc6ea388c8df3f1cda054e81b4d56cf98d2
Zhomart Abumansur <abuka7778888@gmail.com>
2024-10-30 23:22:26 +0600

    HW 31.10

---
+++ HW 31.10/homework 31.10.js
@@ -0,0 +1,11 @@
+var name = "Matiz"
+var color = "Green"
+var wheels = 4
+var Offroad = "false"
+var rating = 5
+
+console.log("Matiz")
+console.log("Green")
+console.log("four")
+console.log("False")
+console.log("five")
