459cb7b97051724fb527cb240ae3688184d7aa21
Zhomart Abumansur <abuka7778888@gmail.com>
2024-10-30 11:51:01 +0600

    about me

---
+++ examples/30.10/about.me.js
@@ -0,0 +1,13 @@
+var name = "Abumansur";
+var surname = "Zhomart";
+var age = 13;
+var weight = 165;
+var height = 44;
+var school ="140";
+
+console.log("At", name)
+console.log("Surname")
+console.log("Age")
+console.log("Weight")
+console.log("Height")
+console.log("School")
