5a08850dc4fe08e2403c52d1ad769e768d3dd981
Zhomart Abumansur <abuka7778888@gmail.com>
2024-10-30 11:06:34 +0600

    example1

---
+++ examples/30.10/js.js
@@ -0,0 +1,11 @@
+var name = "Nazar";
+ 
+name = "Abumansur";
+
+const phone = "192.168.123.10";
+
+phone = "192.172.123.10";
+
+console.log("phone");
+
+document.getElementById("username").innerHTML = name;
