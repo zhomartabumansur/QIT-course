a64f8ae70d12ae267cf807eb08c9da50382e33a1
Zhomart Abumansur <abuka7778888@gmail.com>
2024-11-05 22:58:07 +0600

    home work 04 11

---
+++ HW 31.10/homework04.11.js
@@ -0,0 +1,22 @@
+var age = prompt("жасыңызды жазыңыз")
+var num = 20;
+var temp = prompt("температура")
+var number = 1;
+
+if (number % 2 == 0) {
+    console.log("san on")
+}
+
+if (num % 2 == 0) {
+    console.log("Jup")
+} else {
+    console.log("tak");
+}
+
+if (age <=16) {
+    console.log("Kire almaisyz");
+} else {
+    console.log("Kire alasuz")
+}
+
+ 
