af2f77f2f2ad8d7a911ab8e2daaab390425aef28
Zhomart Abumansur <abuka7778888@gmail.com>
2024-11-05 13:32:05 +0600

    Homework 31.10

---
+++ homeworks/HW 31.10.js
@@ -0,0 +1,13 @@
+var slicesPerPizza = 8;
+var  numberOfPizza = 5;
+
+let totalSlices = "slicesPerPizza * numberOfPizzas"
+console.log(totalSlices)
+
+
+var Chocolates = 24;
+var numberOfFriends = 6;
+
+let chocolatesPerFriend = "totalChocolates / numberOfFriends"
+console.log(chocolatesPerFriend)
+
