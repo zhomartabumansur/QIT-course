23d75100e8d627405e74954ca97e71ed10924944
Zhomart Abumansur <abuka7778888@gmail.com>
2024-11-26 21:57:34 +0600

    homework

---
+++ homeworks/hw26.11.js
@@ -0,0 +1,13 @@
+let arr = []
+ 
+while(true) {
+    let num = +prompt("San jazynyz:")
+
+    if (num % 2 == 0) {
+        alert("San jup")
+    } else {
+        break
+    }
+}
+
+console.log(arr)
